42ee25ebcbcfae3eef226e9ea1e2372afed473ec
JP Rivera <jp@Mac.home.local>
2025-06-19 18:00:10 -0500

    add regions plugin

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import WaveSurfer from 'wavesurfer.js'
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.esm.js'
 
-const wavesurfer = WaveSurfer.create({
-    container: '#waveform',
-    waveColor: '#4F4A85',
-    progressColor: '#383351',
-    url: '../public/walk_insta.wav',
+const regions = RegionsPlugin.create()
+
+const ws = WaveSurfer.create({
+  container: '#waveform',
+  waveColor: 'rgb(200, 0, 200)',
+  progressColor: 'rgb(100, 0, 100)',
+  url: '../public/walk_insta.wav',
+  plugins: [regions],
 })
 
+const random = (min, max) => Math.random() * (max - min) + min
+const randomColor = () => `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, 0.5)`
 
+ws.on('decode', () => {
+  // Regions
+  regions.addRegion({
+    start: 0,
+    end: 8,
+    content: 'Resize me',
+    color: randomColor(),
+    drag: false,
+    resize: true,
+  })
+})
