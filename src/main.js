fdadbafed590a6619849c23b6d8c226fab1dcd8c
JP Rivera <jp@Mac.home.local>
2025-06-21 09:54:13 -0500

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,71 +1,71 @@
-import WaveSurfer from 'wavesurfer.js'
-import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.esm.js'
-import ZoomPlugin from 'wavesurfer.js/dist/plugins/zoom.esm.js'
+import WaveSurfer from "wavesurfer.js";
+import RegionsPlugin from "wavesurfer.js/dist/plugins/regions.esm.js";
+import ZoomPlugin from "wavesurfer.js/dist/plugins/zoom.esm.js";
 
-const regions = RegionsPlugin.create()
+const regions = RegionsPlugin.create();
 
 const ws = WaveSurfer.create({
-  container: '#waveform',
-  waveColor: 'rgb(200, 0, 200)',
-  progressColor: 'rgb(100, 0, 100)',
-  url: '../public/walk_insta.wav',
+  container: "#waveform",
+  waveColor: "rgb(200, 0, 200)",
+  progressColor: "rgb(100, 0, 100)",
+  url: "../public/walk_insta.wav",
   plugins: [regions],
-})
+});
 
-const playButton = document.getElementById('play')
-const pauseButton = document.getElementById('pause')
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
 
-playButton.addEventListener('click', () => {
-    ws.play()
-})
+playButton.addEventListener("click", () => {
+  ws.play();
+});
 
-pauseButton.addEventListener('click', () => {
-    ws.pause()
-})
+pauseButton.addEventListener("click", () => {
+  ws.pause();
+});
 
 ws.registerPlugin(
-    ZoomPlugin.create({
-      scale: 0.5,
-      maxZoom: 100,
-    }),
-)
+  ZoomPlugin.create({
+    scale: 0.5,
+    maxZoom: 100,
+  })
+);
 
-const slider = document.querySelector('#slider');
+const slider = document.querySelector("#slider");
 
-slider.addEventListener('input', (event) => {
+slider.addEventListener("input", (event) => {
   ws.zoom(Number(event.target.value));
 });
 
-const startButton = document.querySelector('#start')
-const endButton = document.querySelector('#end')
-const startValue = document.querySelector('#start-value')
-const endValue = document.querySelector('#end-value')
+const startButton = document.querySelector("#start");
+const endButton = document.querySelector("#end");
+const startValue = document.querySelector("#start-value");
+const endValue = document.querySelector("#end-value");
 
-startButton.addEventListener('click', () => {
-    startValue.value = ws.getCurrentTime()
-})
+startButton.addEventListener("click", () => {
+  startValue.value = ws.getCurrentTime();
+});
 
-endButton.addEventListener('click', () => {
-    endValue.value = ws.getCurrentTime()
-})
+endButton.addEventListener("click", () => {
+  endValue.value = ws.getCurrentTime();
+});
 
 const createPad = (padButtonId) => {
-    regions.addRegion({
-        id: padButtonId,
-        start: startValue.value,
-        end: endValue.value,
-        content: 'Resize me',
-        color: '#000',
-        drag: true,
-        resize: true,
-      })
-      console.log(regions.regions)
-}
+  regions.addRegion({
+    id: padButtonId,
+    start: startValue.value,
+    end: endValue.value,
+    content: "Resize me",
+    color: "#000",
+    drag: true,
+    resize: true,
+  });
+  console.log(regions.regions);
+};
 
-const createPadsButtons = document.querySelectorAll('[id^="create-"]')
+const createPadsButtons = document.querySelectorAll('[id^="create-"]');
 createPadsButtons.forEach((i) => {
-    var createPadButton = document.getElementById(i.id)
-    createPadButton.addEventListener('click', () => {
-        createPad(this.id)
-    })
+  var createPadButton = document.getElementById(i.id);
+  createPadButton.addEventListener("click", () => {
+    createPad(createPadButton.id);
+  });
 });
